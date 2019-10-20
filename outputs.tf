d04369d052be1237fe0c99b2b6c2bd3c46c6e217
recetasdevops <44634828+recetasdevops@users.noreply.github.com>
2019-10-20 10:09:24 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,7 @@ output "sql_user" {
 output "sql_password" {
   value = azurerm_sql_server.DatabaseServerPasionPorLosBits.administrator_login_password
 }
+
+output "sql_connectionstring" {
+  value = "Server=tcp:${azurerm_sql_server.DatabaseServerPasionPorLosBits.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.DatabaseInstancePasionPorLosBits.name};Persist Security Info=False;User ID=${azurerm_sql_server.DatabaseServerPasionPorLosBits.administrator_login};Password=${azurerm_sql_server.DatabaseServerPasionPorLosBits.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+}
